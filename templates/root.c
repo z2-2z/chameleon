e0f3a2b43197349f3b9a92ae3f949d440aa20c6f
z2_ <88509734+z2-2z@users.noreply.github.com>
2026-01-03 05:41:57 +0100

    Using weighted random for sequence mutation

--- templates/root.c
+++ templates/root.c
@@ -50,6 +50,23 @@ static inline size_t internal_random (void) {
     return rand_state = x;
 }
 
+static inline size_t weighted_random (size_t n) {
+    if (UNLIKELY(n < 3)) {
+        return internal_random() % n;
+    }
+    
+    size_t part = n / 3;
+    size_t idx = internal_random() % part;
+    
+    switch (internal_random() % 4) {
+        case 0: return idx;
+        case 1:
+        case 2: return part + idx;
+        case 3: return 2 * part + idx;
+        default: __builtin_unreachable();
+    }
+}
+
 /***** TERMINALS *****/
 
 {% for (id, content) in grammar.terminals() -%}
@@ -190,9 +207,9 @@ EXPORT_FUNCTION
 size_t chameleon_mutate (ChameleonWalk* walk, unsigned char* output, size_t output_length) {
     size_t length = 0;
     if (LIKELY(walk->length > 0)) {
-        length = internal_random() % walk->length;
+        length = weighted_random(walk->length);
+        walk->length = 0;
     }
-    walk->length = 0;
     return _mutate_nonterm_{{ grammar.entrypoint().id() }}(walk->steps, length, walk->capacity, &walk->length, output, output_length);
 }
 #endif /* OMIT_CHAMELEON_MUTATE */
