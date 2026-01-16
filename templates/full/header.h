ade29ef7b643f63d3f63a9cdbb30683622d9dba8
z2_ <88509734+z2-2z@users.noreply.github.com>
2026-01-16 23:59:36 +0100

    Removed guard around typedef in header

--- templates/full/header.h
+++ templates/full/header.h
@@ -3,11 +3,8 @@
 
 #include <stddef.h>
 
-#ifndef _HAVE_CHAMELEON_WALK
-#define _HAVE_CHAMELEON_WALK
 // Details of ChameleonWalk are private to the generated code
 typedef unsigned char ChameleonWalk[32];
-#endif
 
 void {{ prefix }}_seed (size_t new_seed);
 void {{ prefix }}_init (ChameleonWalk walk, size_t capacity);
